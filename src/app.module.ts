4da74c88ec5558ad986fb3b5f841b72d9de2a1f6
aleferraro7 <[alejandroferraro7@gmail.com]>
2024-07-04 19:04:00 +0200

    users module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { DataSourceConfig } from './config/data.source';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
